55a6e727ffda6f37b87193c063c7fa99be5815e1
halyna-kuzminchuk <halkuz01@easv365.dk>
2026-04-27 20:15:21 +0300

    Header and empty css files enqueued in inc

--- wp-content/themes/salon-zig-zag/inc/enqueue.php
+++ wp-content/themes/salon-zig-zag/inc/enqueue.php
@@ -1,18 +1,67 @@
 <?php
-function szz_enqueue_assets() {
-    wp_enqueue_style( 
-        'szz-main',           
-        get_template_directory_uri() . '/assets/css/main.css',           
-        ['typekit', 'montserrat'],                   
-        '1.0.0'               
-    );
+function szz_enqueue_assets()
+{
+
     wp_enqueue_style(
         'typekit',
-         'https://use.typekit.net/srr6mup.css');
-         
-    wp_enqueue_style('montserrat',
-         'https://fonts.googleapis.com/css2?family=Montserrat:wght@400;500&display=swap');
+        'https://use.typekit.net/srr6mup.css'
+    );
+
+    wp_enqueue_style(
+        'montserrat',
+        'https://fonts.googleapis.com/css2?family=Montserrat:wght@400;500&display=swap'
+    );
+
+    wp_enqueue_style(
+        'szz-main',
+        get_template_directory_uri() . '/assets/css/main.css',
+        ['typekit', 'montserrat'],
+        '1.0.0'
+    );
+
+    wp_enqueue_style(
+        'szz-header',
+        get_template_directory_uri() . '/assets/css/header.css',
+        ['szz-main'],
+        '1.0.0'
+    );
+    wp_enqueue_style(
+        'szz-footer',
+        get_template_directory_uri() . '/assets/css/footer.css',
+        ['szz-main'],
+        '1.0.0'
+    );
+    wp_enqueue_style(
+        'szz-buttons',
+        get_template_directory_uri() . '/assets/css/components/buttons.css',
+        ['szz-main'],
+        '1.0.0'
+    );
+    wp_enqueue_style(
+        'szz-service-card',
+        get_template_directory_uri() . '/assets/css/components/service-card.css',
+        ['szz-main'],
+        '1.0.0'
+    );
+    wp_enqueue_style(
+        'szz-about-page',
+        get_template_directory_uri() . '/assets/css/pages/about-page.css',
+        ['szz-main'],
+        '1.0.0'
+    );
+    wp_enqueue_style(
+        'szz-gallery',
+        get_template_directory_uri() . '/assets/css/pages/gallery.css',
+        ['szz-main'],
+        '1.0.0'
+    );
+    wp_enqueue_style(
+        'szz-priser-page',
+        get_template_directory_uri() . '/assets/css/pages/priser-page.css',
+        ['szz-main'],
+        '1.0.0'
+    );
 }
 
 
-add_action( 'wp_enqueue_scripts', 'szz_enqueue_assets');
+add_action('wp_enqueue_scripts', 'szz_enqueue_assets');
