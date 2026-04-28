c43de214412c991c1e5184d2f13bb9664fc26a62
Tove2812 <tovdeb01@easv365.dk>
2026-04-28 11:33:43 +0200

    Styling of WooCommerce Part 1 (not fully done)

--- wp-content/themes/salon-zig-zag/functions.php
+++ wp-content/themes/salon-zig-zag/functions.php
@@ -2,3 +2,7 @@
 require_once get_template_directory() . '/inc/enqueue.php';
 
 require_once get_template_directory() . '/inc/setup.php';
+
+add_filter('woocommerce_product_add_to_cart_text', function() {
+    return 'Tilføj til kurv';
+});
